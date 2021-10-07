9b937d140706934596166cfe1707267f0811d1cf
unknown <mccullochryan636@gmail.com>
2021-10-06 19:35:38 -0600

    Trying Seatgeek API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
-var ticketmaster = "ma4xCtfbVX6HAL5AbR0JZjnhfwzzFMOy"
-var ticketmasterURL = 'https://cors-anywhere.herokuapp.com/https://app.ticketmaster.com/discovery/v2/events.json?size=1&postalCode=80228&apikey=${ticketmaster}'
+// var ticketmaster = "ma4xCtfbVX6HAL5AbR0JZjnhfwzzFMOy"
+// var ticketmasterURL = 'https://cors-anywhere.herokuapp.com/https://app.ticketmaster.com/discovery/v2/events.json?size=1&postalCode=80228&apikey=${ticketmaster}'
+// var sportsAPI = "cd6116b0-264b-11ec-b2e9-e52c4e87d97a"
+
+// var seatGeek = "4a649bd858de54c13c57fa3341d39a5858fd79d148f3fec66b521dea9a881bd1"
+var seatGeekUrl = "https://api.seatgeek.com/2/events?client_id=MjM3ODMwNDN8MTYzMzU2OTE4MC42OTA3NDk0"
+var seatGeek = 
 $("#search").on("click" , function(){
     var zipcode = $("#Zipput").val()
     console.log(zipcode)
@@ -7,7 +12,7 @@ $("#search").on("click" , function(){
 
 $.ajax({
     type:"GET",
-    url:'https://app.ticketmaster.com/discovery/v2/events.json?${ticketmaster}',
+    url:'https://api.seatgeek.com/2/events?client_id=MjM3ODMwNDN8MTYzMzU2OTE4MC42OTA3NDk0',
     async:true,
     dataType: "json",
     success: function(json) {

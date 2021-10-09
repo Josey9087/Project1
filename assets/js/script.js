75e290f6a30e5a89692307c12f60c2fbf845b56f
ZaymonDgon <zaydmonkey@gmail.com>
2021-10-09 10:36:42 -0600

    addeed today's date into the title

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 // var ticketmaster = "ma4xCtfbVX6HAL5AbR0JZjnhfwzzFMOy"
 // var ticketmasterURL = 'https://cors-anywhere.herokuapp.com/https://app.ticketmaster.com/discovery/v2/events.json?size=1&postalCode=80228&apikey=${ticketmaster}'
 // var sportsAPI = "cd6116b0-264b-11ec-b2e9-e52c4e87d97a"
-
 // var seatGeek = "4a649bd858de54c13c57fa3341d39a5858fd79d148f3fec66b521dea9a881bd1"
+var today = moment();
+$('#today').text(today.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+
 var seatGeekUrl = "https://api.seatgeek.com/2/events?client_id=MjM3ODMwNDN8MTYzMzU2OTE4MC42OTA3NDk0"
 var seatGeek = 
 $("#search").on("click" , function(){
